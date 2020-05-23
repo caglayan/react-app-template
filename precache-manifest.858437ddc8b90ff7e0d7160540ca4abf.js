45c1abbdeb3c411f2163f2a2ca459e79d5f607c3
caglayan <caglayanserbetci@gmail.com>
2020-05-23 23:37:09 +0300

    Updates

---
+++ precache-manifest.858437ddc8b90ff7e0d7160540ca4abf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb53952b46487162d46cb80fd7fe1eb",
+    "url": "/react-app-template/index.html"
+  },
+  {
+    "revision": "b24c2e364475823c885f",
+    "url": "/react-app-template/static/css/main.7838dbdf.chunk.css"
+  },
+  {
+    "revision": "5c8e689323c781499bbe",
+    "url": "/react-app-template/static/js/2.7c8fe555.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/react-app-template/static/js/2.7c8fe555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b24c2e364475823c885f",
+    "url": "/react-app-template/static/js/main.d9e18e02.chunk.js"
+  },
+  {
+    "revision": "0567463811194a5b763c",
+    "url": "/react-app-template/static/js/runtime-main.10f43903.js"
+  },
+  {
+    "revision": "57ba8a391ea0d9c36079cafd4edcebb2",
+    "url": "/react-app-template/static/media/logo.57ba8a39.svg"
+  }
+]);
